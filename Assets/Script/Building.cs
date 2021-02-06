65773ff70250d4db34dbd74ffebdf379ce165568
Harriet Lee <lharriet27@gmail.com>
2021-02-06 18:41:07 +0900

    created structure of tower inherit

---
+++ Assets/Script/Building.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    protected int unitNum;
+    protected int maxUnit = 100;
+    protected float delay;
+    protected float spawnTime = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+    }
+
+    protected void SpawnUnit()
+    {
+        if (maxUnit < unitNum) return;
+
+        if (spawnTime > delay)
+        {
+            spawnTime = 0f;
+            unitNum += 1;
+        }
+    }
+}
---
+++ Assets/Script/DefenseTower.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenseTower : Building
+{
+    //공격하는 타워
+    
+    public GameObject bombPref;
+    public Transform firePos;
+    
+    //타겟이 범위에 들어오면 유닛 공격(우리팀이 아닐 경우)
+    public float range;
+    // Start is called before the first frame update
+    void Start()
+    {
+        range = 5f;
+        delay = 3f;
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        base.Update();
+        spawnTime += Time.deltaTime;
+        SpawnUnit();
+    }
+}
