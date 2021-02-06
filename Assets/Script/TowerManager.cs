bcdc612500db67994208e424ef12e9760d91a155
Harriet Lee <lharriet27@gmail.com>
2021-02-06 16:15:39 +0900

    moved project files for connection with github

---
+++ Assets/Script/TowerManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerManager : MonoBehaviour
+{
+    public enum myTower
+    {
+        RED_TOWER, ORANGE_TOWER
+    }
+
+    //public int maxCount;
+    public GameObject[] towerObj = new GameObject[2];
+    public Transform[] place = new Transform[2];
+
+    myTower tower;
+    int towerKind;
+
+    private void Awake()
+    {
+
+
+    }
+    void Start()
+    {
+        towerKind = Random.Range(0, 2);
+        tower = (myTower)towerKind;
+
+        Instantiate(towerObj[(int)tower], place[(int)tower].position, Quaternion.identity);
+
+    }
+
+    void Update()
+    {
+    }
+
+    public int TowerNumber
+
+    {
+        get { return towerKind; }
+        set { towerKind = value; }
+    }
+
+}
