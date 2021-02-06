25443041a695728e5f5051346fc4f5f3eb6335a8
Harriet Lee <lharriet27@gmail.com>
2021-02-06 20:22:51 +0900

    created scripts of defense tower and house tower

--- Assets/Script/Building.cs
+++ Assets/Script/Building.cs
@@ -3,20 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Building : MonoBehaviour
-{
+{ 
+    public enum TOWERKIND
+    {
+        HOUSE, DEFENSE, CASTLE
+    }
+    public TOWERKIND kind;
+
     protected int unitNum;
     protected int maxUnit = 100;
+    protected int level;
     protected float delay;
     protected float spawnTime = 0f;
-    // Start is called before the first frame update
-    void Start()
+
+    protected TextMesh showText;
+
+    protected virtual void Awake()
+    {
+        showText = GetComponentInChildren<TextMesh>();
+    }
+    protected virtual void Start()
     {
 
+        showText.text = unitNum.ToString();
     }
 
     // Update is called once per frame
     protected virtual void Update()
     {
+     
     }
 
     protected void SpawnUnit()
@@ -27,6 +42,28 @@ public class Building : MonoBehaviour
         {
             spawnTime = 0f;
             unitNum += 1;
+            showText.text = unitNum.ToString();
+        }
+    }
+
+    protected void InitSpawnUnit (float spawnDelay,int count= 0)
+    {
+        delay = spawnDelay;
+        unitNum = count;
+    }
+    
+    public IEnumerator DragObject()
+    {
+        Vector3 screen = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 offset = transform.position - Camera.main.ScreenToWorldPoint
+            (new Vector3(Input.mousePosition.x, Input.mousePosition.y, screen.z));
+
+        while(Input.GetMouseButton(0))
+        {
+            Vector3 curScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screen.z);
+            Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenSpace) + offset;
+            transform.position = curPosition;
+            yield return null;
         }
     }
 }
--- Assets/Script/DefenseTower.cs
+++ Assets/Script/DefenseTower.cs
@@ -2,27 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class DefenseTower : Building
 {
-    //공격하는 타워
+    //공격하는 타워 (default- 팀에 속하지 않음)
     
     public GameObject bombPref;
     public Transform firePos;
+    public GameObject target;
     
     //타겟이 범위에 들어오면 유닛 공격(우리팀이 아닐 경우)
     public float range;
-    // Start is called before the first frame update
-    void Start()
-    {
-        range = 5f;
-        delay = 3f;
-    }
+    //팀에 속하지 않을 때, 가장 먼저  hp만큼의 유닛을 보낸 팀이 공격하는 타워를 선점가능
+    public int hp;
 
-    // Update is called once per frame
+
+ 
+    protected override void Awake()
+    {
+        base.Awake();
+        hp = 5;
+        range = 5f;
+        InitSpawnUnit(2f);
+    }
+    protected override void Start()
+    {
+        base.Start();
+    }
     protected override void Update()
     {
         base.Update();
-        spawnTime += Time.deltaTime;
-        SpawnUnit();
+        if(hp<=0)
+        {
+            hp = 0;
+            spawnTime += Time.deltaTime;
+            SpawnUnit();
+        }
+    }
+
+    public void attack()
+    {
+
     }
 }
