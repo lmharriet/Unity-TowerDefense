8d5e51b458ad18b7122357083d0b2fb1e6d91916
Harriet Lee <lharriet27@gmail.com>
2021-03-22 23:48:14 +0900

    doc : deco Lobby Scene

---
+++ Assets/H_assets/test.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    int randomSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomSpeed = Random.Range(10, 30);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.GetChild(0).Rotate(0, -25 * randomSpeed * Time.deltaTime, 0);
+
+        transform.Translate(Vector3.left * randomSpeed * Time.deltaTime);
+    }
+}
