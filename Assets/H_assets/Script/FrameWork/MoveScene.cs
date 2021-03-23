86ec7660d16f55298fc5bc73cad4ea63470bc9a3
Harriet Lee <lharriet27@gmail.com>
2021-03-23 23:45:39 +0900

    feat : LoadScene with SceneMaanger

---
+++ Assets/H_assets/Script/FrameWork/MoveScene.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MoveScene : Singleton<MoveScene>
+{
+    protected MoveScene() { }
+    public Image loadingImage;
+    public Image progressBar;
+
+    private string loadSceneName;
+    void Start()
+    {
+
+    }
+
+    private void OnDisable()
+    {
+        //StopCoroutine(loadingBar());
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        loadSceneName = sceneName;
+        loadingImage.gameObject.SetActive(true);
+        StartCoroutine(loadingBar());
+
+    }
+
+    IEnumerator loadingBar()
+    {
+
+        Time.timeScale = 1;
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(loadSceneName);
+
+        yield return new WaitForSeconds(1f);
+        loadingImage.gameObject.SetActive(false);
+        Time.timeScale = 0;
+        //gameObject.SetActive(true);
+    }
+}
--- Assets/H_assets/Script/UI/StopPanel.cs
+++ Assets/H_assets/Script/UI/StopPanel.cs
@@ -7,18 +7,18 @@ public class StopPanel : MonoBehaviour
     [SerializeField]
     GameObject stopPopUp;
 
-    int touchCount;
+    //int touchCount;
   
     public void ActivePopUp()
     {
         stopPopUp.SetActive(true);
-        touchCount = 1;
+        //touchCount = 1;
         Time.timeScale = 0;
     }
     public void DeActivePopUp()
     {
         stopPopUp.SetActive(false);
-        touchCount = 0;
+       //touchCount = 0;
         Time.timeScale = 1;
     }
 }
